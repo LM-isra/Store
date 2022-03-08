03fc5fb7513b24b2aaae6b760688aea42df342d9
Israel Silva_QK <israel.silva@qacg.com>
2022-03-08 13:32:52 -0600

    End of section 12

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHearderComponent } from './components/paging-hearder/paging-hearder.component';
 import { PagerComponent } from './components/pager/pager.component';
+
 
 @NgModule({
   declarations: [
@@ -11,12 +13,14 @@ import { PagerComponent } from './components/pager/pager.component';
   ],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [ 
     PaginationModule,
     PagingHearderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
